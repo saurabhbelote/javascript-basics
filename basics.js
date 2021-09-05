1e5e9c222b6732e190af500d1c4f2c6c476810cb
saurabh belote <saurabhbelote112@gmail.com>
2021-09-05 03:23:31 -0700

    first commit

---
+++ basics.js
@@ -0,0 +1,53 @@
+//comments
+console.log("hello world!")
+
+//variable = store data
+
+let firstName = 'saurabh'
+let lastName = 'belote'
+firstName = 'shruti'
+
+console.log(firstName, lastName)
+
+var yourName
+
+yourName = 'sulochana'
+
+console.log(yourName)
+
+// constant = store data can not be changed
+
+
+const myFullName = 'kingsAI'
+
+console.log(myFullName)
+
+//variable naming rules
+// 1. No space
+// 2. Must begin with _,$ or a letter
+// 3. Can contain letter ,number ,underscore and doller sign
+// 4. Case Sensitive
+
+
+// add two strings
+let fName = "subham"
+let lName = 'saurah'
+
+let myName = fName + '  '+ lName
+console.log(myName.length)
+
+let fulname = `${fName} ${lName} is my full name`
+console.log(fulname)
+
+// arrays
+
+let fruits = ['apple','mango','banana']
+
+let codinglanguage = [ 'javascript', 'python','go']
+
+console.log(codinglanguage[0])
+codinglanguage.push('php')
+console.log(codinglanguage)
+codinglanguage.unshift('php')
+console.log(codinglanguage)
+
